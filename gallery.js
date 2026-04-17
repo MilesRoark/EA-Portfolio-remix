a402f2b35b1efa7807ba6b81dfed4cdd68d3315e
milesroark <babatunjia@gmail.com>
2026-04-17 02:45:41 -0400

    heading in a new direction
    -reworking the experiences page.
    -added the images for the gallery page.

---
+++ gallery.js
@@ -0,0 +1,6 @@
+const dialog = document.getElementById("myDialog");
+const openBtn = document.getElementById("openBtn");
+
+openBtn.addEventListener("click", () => {
+  dialog.showModal(); // Opens as a modal
+});
