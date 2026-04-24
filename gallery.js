ec601ac4b28675aade4a88aece970560397f9090
milesroark <babatunjia@gmail.com>
2026-04-24 03:08:32 -0400

    slideshow implemented
    -added a slideshow to the galllery.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,20 @@
-const dialog = document.getElementById("myDialog");
-const openBtn = document.getElementById("openBtn");
+const backArrow = document.querySelector(".back");
+const forwardArrow = document.querySelector(".forward");
+const displayedImage = document.querySelector(".image1");
 
-openBtn.addEventListener("click", () => {
-  dialog.showModal(); // Opens as a modal
+const images = [];
+for (let i = 1; i <= 30; i++) {
+  images.push(`Assets/Gallery-${i}.webp`);
+}
+
+let currentIndex = 0;
+
+backArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  displayedImage.src = images[currentIndex];
+});
+
+forwardArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  displayedImage.src = images[currentIndex];
 });
